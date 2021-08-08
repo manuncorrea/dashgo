b4bcedb3dc7e926e36196cb5bc649f38673d7789
manuncorrea <manuncorrea@hotmail.com>
2021-08-08 11:36:06 -0300

    creating the sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,15 @@
+import { Flex } from "@chakra-ui/react";
 import { Header } from "../components/Header";
+import { Sidbar } from "../components/Sidbar";
 
 export default function Dashboard() {
   return (
-    <Header />
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex width="100%" marginY="6" maxWidth={1480} mx="auto" paddingX="6">
+        <Sidbar />
+      </Flex>
+    </Flex>
   )
 }

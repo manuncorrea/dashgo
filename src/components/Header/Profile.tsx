baa5aedd107ef924b4ebcd32edd0b543b4f486df
manuncorrea <manuncorrea@hotmail.com>
2021-09-04 20:32:01 -0300

    header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,26 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
-  return(
-    <Flex align="center">
-    <Box marginRight="4" textAlign="right">
-        <Text>Emanuele Correa</Text>
-        <Text color="gray.300" fontSize="small">manu@gmail.com</Text>
-      </Box>
+interface ProfileProps {
+  showProfileData?: boolean;
+}
 
-      <Avatar size="md" name="Emanuele Correa" src="https://github.com/manuncorrea.png" />
+export function Profile({ showProfileData = true }: ProfileProps) {
+  return (
+    <Flex align="center">
+      {showProfileData && (
+        <Box marginRight="4" textAlign="right">
+          <Text>Emanuele Correa</Text>
+          <Text color="gray.300" fontSize="small">
+            manu@gmail.com
+          </Text>
+        </Box>
+      )}
+
+      <Avatar
+        size="md"
+        name="Emanuele Correa"
+        src="https://github.com/manuncorrea.png"
+      />
     </Flex>
-  )
+  );
 }

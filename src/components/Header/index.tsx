f18f92d8c5abb9cee6472cf75f7421406b7532a6
manuncorrea <manuncorrea@hotmail.com>
2021-09-05 09:56:10 -0300

    sidebar responsivo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,33 +1,48 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, useBreakpointValue, IconButton } from '@chakra-ui/react';
+import { RiMenuLine } from 'react-icons/ri';
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext';
 import { Logo } from "./Logo";
 import { NotificationNav } from "./NotificationNav";
 import { Profile } from "./Profile";
 import { SearchBox } from "./SearchBox";
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer()
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true, 
   })
   return (
-    <Flex
+    <Flex 
       as="header"
       width="100%"
-      maxWidth={1480}
-      height="20"
-      marginX="auto"
+      maxWidth={1480} 
+      height="20" 
+      marginX="auto" 
       marginTop="4"
-      paddingX="6"
+      paddingX="6" 
       align="center"
     >
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          marginRight="2"
+       />
+      )}
+       
       <Logo />
-
       { isWideVersion && <SearchBox /> }
+
       <Flex align="center" ml="auto">
         <NotificationNav />
-
-        <Profile  showProfileData={isWideVersion}/>
+        <Profile showProfileData={isWideVersion} />
       </Flex>
+      
     </Flex>
   );
 }
--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,6 +1,6 @@
 import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
 import { Header } from "../../components/Header";
-import { Sidbar } from "../../components/Sidbar";
+import { Sidebar } from "../../components/Sidebar";
 
 import {Input} from "../../components/Form/Input";
 export default function CreateUser() {
@@ -9,20 +9,20 @@ export default function CreateUser() {
       <Header />
 
       <Flex width="100%" marginY="6" maxWidth={1480} marginX="auto" paddingX="6">
-        <Sidbar />
+        <Sidebar />
 
-        <Box flex="1" borderRadius={8} backgroundColor="gray.800" padding="8" >
+        <Box flex="1" borderRadius={8} backgroundColor="gray.800" padding={["6", "8" ]}>
           <Heading size="lg" fontWeight="normal">Criar usuario</Heading>
 
           <Divider my="6" borderColor="gray.700"></Divider>
 
-          <VStack spacing="8">
+          <VStack spacing={["6", "8" ]}>
             <SimpleGrid minChildWidth="240px" spacing="8" width="100%">
               <Input name="name" label="Nome completo"/>
               <Input name="email" type="email" label="E-mail"/>
             </SimpleGrid>
 
-            <SimpleGrid minChildWidth="240px" spacing="8" width="100%">
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8" ]} width="100%">
               <Input name="password" type="password" label="Senha"/>
               <Input name="password_confirmation" type="password" label="Confirmação da Senha"/>
             </SimpleGrid>

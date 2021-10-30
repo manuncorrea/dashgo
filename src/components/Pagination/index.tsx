a697a4feec569120835c22ca6b994758143425e2
manuncorrea <manuncorrea@hotmail.com>
2021-10-30 09:30:02 -0300

    componente de paginaçao

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,7 +1,42 @@
-import { Box, Stack } from "@chakra-ui/layout";
+import { Box, Stack, Text } from "@chakra-ui/layout";
 import { PaginationItem } from "./PaginationItem";
 
-export function Pagination () {
+interface PaginationProps {
+  totalCountOfRegisters: number;
+  registersPerPage?: number;
+  currentPage?: number;
+  onPageChange: (page: number) => void;
+
+}
+
+const siblingsCount = 1;
+
+generetePagesArray(2, 5);
+function generetePagesArray(from: number, to: number) {
+  return [...new Array(to - from)]
+    .map((_, index) => {
+      return from + index + 1;
+    })
+    .filter(page => page > 0)
+}
+
+export function Pagination ({
+  totalCountOfRegisters,
+  registersPerPage = 10,
+  currentPage = 1,
+  onPageChange
+}: PaginationProps) {
+  const lastPage =  Math.floor(totalCountOfRegisters / registersPerPage);
+
+
+  const previousPages = currentPage > 1
+  ? generetePagesArray(currentPage - 1 - siblingsCount, currentPage -1 )
+  : []
+
+  const nextPages = currentPage < lastPage
+  ? generetePagesArray(currentPage,  Math.min(currentPage + siblingsCount, lastPage))
+  : []
+
   return(
     <Stack 
       direction={["column", "row"]} 
@@ -14,10 +49,34 @@ export function Pagination () {
         <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
       </Box>
       <Stack direction="row" spacing="2">
-        <PaginationItem number={1} isCurrent />
-        <PaginationItem number={2} />
-        <PaginationItem number={3} />
-        <PaginationItem number={4} />
+
+        {currentPage > (1 + siblingsCount ) && (
+          <>
+            <PaginationItem number={1} />
+            { currentPage > (2 + siblingsCount) && 
+             <Text color='gray.300' width='8' textAlign='center'>...</Text>
+            }
+          </>
+        )}
+
+        {previousPages.length > 0 && previousPages.map(page => {
+          return <PaginationItem key={page} number={page} />
+        })}
+
+        <PaginationItem number={currentPage} isCurrent />
+
+        {nextPages.length > 0 && nextPages.map(page => {
+          return <PaginationItem key={page} number={page} />
+        })}
+
+        {(currentPage + siblingsCount) < lastPage && (
+          <>
+          {( currentPage + 1 + siblingsCount) < lastPage && 
+           <Text color='gray.300' width='8' textAlign='center'>...</Text>
+          }
+            <PaginationItem number={lastPage}   />
+          </>
+        )}
       </Stack>
     </Stack>
   );
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -14,96 +14,102 @@ import {
   Text,
   useBreakpointValue,
   Spinner,
-} from '@chakra-ui/react';
-import Link from 'next/link';
+} from "@chakra-ui/react";
+import Link from "next/link";
+import { useState } from "react";
 
-import { RiAddLine } from 'react-icons/ri';
-import { Header } from '../../components/Header';
-import { Pagination } from '../../components/Pagination';
-import { Sidebar } from '../../components/Sidebar';
-import { useUsers } from '../../services/hooks/useUser';
+import { RiAddLine } from "react-icons/ri";
+import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
+import { Sidebar } from "../../components/Sidebar";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useUsers();
+  const [page, setPage] = useState(1)
+  const { data, isLoading, isFetching, error } = useUsers(page)
 
   const isWideVersion = useBreakpointValue({
     base: false,
-    lg: true,
-  });
+    lg: true
+  })
   return (
     <Box>
       <Header />
       <Flex
-        width='100%'
-        marginY='6'
+        width="100%"
+        marginY="6"
         maxWidth={1480}
-        marginX='auto'
-        paddingX='6'
+        marginX="auto"
+        paddingX="6"
       >
         <Sidebar />
 
-        <Box flex='1' borderRadius={8} background='gray.800' padding='8'>
-          <Flex marginBottom='8' justifyContent='space-between' align='center'>
-            <Heading size='lg' fontWeight='normal'>
-              Usuario
-              { !isLoading && isFetching && <Spinner size='sm' color='gray.500' ml='4' /> }
+        <Box flex="1" borderRadius={8} background="gray.800" padding="8">
+          <Flex marginBottom="8" justifyContent="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
 
-            <Link href='/users/create' passHref>
+            <Link href="/users/create" passHref>
               <Button
-                as='a'
-                size='sm'
-                fontSize='sm'
-                colorScheme='pink'
-                leftIcon={<Icon as={RiAddLine} fontSize='20' />}
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
               >
                 Criar novo
               </Button>
             </Link>
           </Flex>
           { isLoading ? (
-            <Flex justify='center'>
+            <Flex justify="center">
               <Spinner />
             </Flex>
           ) : error ? (
-            <Flex justify='center'>
+            <Flex justify="center">
               <Text>Falha ao obter dados dos usuários</Text>
             </Flex>
           ) : (
             <>
-              <Table colorScheme='whiteAlpha'>
+              <Table colorScheme="whiteAlpha">
                 <Thead>
                   <Tr>
-                    <Th px={['4', '4', '6']} color='gray.300' width='8'>
-                      <Checkbox colorScheme='pink' />
+                    <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
                     </Th>
                     <Th>Usuário</Th>
                     {isWideVersion && <Th>Data de cadastro</Th>}
-                    <Th width='8'></Th>
+                    <Th width="8"></Th>
                   </Tr>
                 </Thead>
                 <Tbody>
-      
-                {data.map(user => {
-                  return(
+                  {data.users.map(user => (
                     <Tr key={user.id}>
-                    <Td px={['4', '4', '6']}>
-                      <Checkbox colorScheme='pink' />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight='bold'>{user.name}</Text>
-                        <Text fontSize='sm' color='gray.300'>{user.email}</Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>{user.createdAt}</Td>}
-                  </Tr>
-                  )
-                })}
-                  
+                      <Td px={["4", "4", "6"]}>
+                        <Checkbox colorScheme="pink" />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight="bold">{user.name}</Text>
+                          <Text fontSize="sm" color="gray.300">
+                            {user.email}
+                          </Text>
+                        </Box>
+                      </Td>
+                      {isWideVersion && <Td>{user.createdAt}</Td>}
+                    </Tr>
+                  ))}
                 </Tbody>
               </Table>
-              <Pagination />
+              <Pagination
+                totalCountOfRegisters={200}
+                currentPage={5}
+                onPageChange={() => {}}
+              />
             </>
           )}
         </Box>
